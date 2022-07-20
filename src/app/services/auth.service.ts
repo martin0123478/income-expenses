4f1a2ca5f0f13adc9a803707cf8bb931bdbe4246
martin0123478 <martin_agui97@outlook.es>
2022-07-20 13:41:48 -0500

    obtener datos de usuario,protección de rutas, guardar datos de usuario en bd

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs/operators';
+import { Usuario } from '../models/usuario.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public auth: AngularFireAuth) { }
+  constructor(public auth: AngularFireAuth,
+    private firestore:AngularFirestore) { }
+
+  initAuthListener(){
+    this.auth.authState.subscribe(fuser=>{
+      console.log(fuser?.uid)
+      console.log(fuser?.email)
+    })
+  }
+
   crearUsuario(nombre:string,email:string,password:string){
-    //console.log(nombre,email,password)
    return  this.auth.createUserWithEmailAndPassword(email,password)
+   .then(({user})=>{
+    const newUser = new Usuario(user?.uid,nombre,email)
+   return this.firestore.doc(`${user?.uid}/usuario`)
+      .set({...newUser})
+   })
   }
   loginUsuario(email:string,password:string){
     return this.auth.signInWithEmailAndPassword(email,password)
@@ -17,5 +33,11 @@ export class AuthService {
 
   logout(){
    return this.auth.signOut()
+  }
+
+  isAuth(){
+    return this.auth.authState.pipe(
+      map(fuser => fuser != null)
+    ) 
   }
 }
